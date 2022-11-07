38cca39d48543104dc9bfe7de077e81c0a424363
Tonya <tonyakostova@hotmail.com>
2022-11-07 13:07:39 +0200

    Commiting again

--- src/PLayground.java
+++ src/PLayground.java
@@ -2,5 +2,6 @@ public class PLayground {
     public static void main(String[] args) {
         System.out.println("Playing with GIT");
         System.out.println("I hope I understand it well enough!");
+        String new1 = "Are you mega sure";
     }
 }
