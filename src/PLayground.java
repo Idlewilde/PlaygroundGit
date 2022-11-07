829f4320edcd47361bdeae6243d7a63e4c1f810d
Tonya <tonyakostova@hotmail.com>
2022-11-07 12:52:12 +0200

    Trying it like that

--- src/PLayground.java
+++ src/PLayground.java
@@ -1,5 +1,6 @@
 public class PLayground {
     public static void main(String[] args) {
         System.out.println("Playing with GIT");
+        System.out.println("I hope I understand it well enough!");
     }
 }
